bfdbb3fdc16363e117aafd762b61c5065cec9617
KhacSon0907 <creatine09072004@gmail.com>
2025-03-01 13:24:38 +0700

    ok gui ik

---
+++ app/src/main/java/com/example/bottomnav/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.bottomnav;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.action_home:{
+                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                    }
+                    case R.id.action_favorite:{
+                        Toast.makeText(MainActivity.this,"Favorite",Toast.LENGTH_SHORT).show();
+                    } case R.id.action_my_page:{
+                        Toast.makeText(MainActivity.this,"My page",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                return  true;
+            }
+        });
+    }
+}
